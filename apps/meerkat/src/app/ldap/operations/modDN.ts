d3ed9d6f913ef2c81f981778b2e878f197746edc
Jonathan M. Wilbur <jonathan@wilbur.space>
2021-06-25 22:52:30 -0400

    Implement deleteoldrdn
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- apps/meerkat/src/app/ldap/operations/modDN.ts
+++ apps/meerkat/src/app/ldap/operations/modDN.ts
@@ -1,4 +1,4 @@
-import type { Context } from "../../types";
+import { Context, IndexableOID } from "../../types";
 import type {
     ModifyDNRequest,
 } from "@wildboar/ldap/src/lib/modules/Lightweight-Directory-Access-Protocol-V3/ModifyDNRequest.ta";
@@ -9,9 +9,11 @@ import {
     LDAPResult,
 } from "@wildboar/ldap/src/lib/modules/Lightweight-Directory-Access-Protocol-V3/AddResponse.ta";
 import findEntry from "../../x500/findEntry";
+import readEntry from "../../database/readEntry";
 import decodeLDAPDN from "../decodeLDAPDN";
 import rdnToJson from "../../x500/rdnToJson";
 import { objectNotFound } from "../results";
+import { ASN1Construction } from "asn1-ts";
 
 // ModifyDNRequest ::= [APPLICATION 12] SEQUENCE {
 //     entry           LDAPDN,
@@ -29,6 +31,7 @@ async function modDN (
     if (!entry) {
         return objectNotFound;
     }
+    const oldrdn = entry.rdn;
     const newrdn = decodeLDAPDN(ctx, req.newrdn)[0];
     if (req.newSuperior) {
         const newSuperiorDN = decodeLDAPDN(ctx, req.newSuperior);
@@ -61,6 +64,71 @@ async function modDN (
             },
         });
     }
+
+
+    if (req.deleteoldrdn) {
+        const attributeTypesInOldRDN = new Set<IndexableOID>(
+            oldrdn.map((atav) => atav.type_.toString()),
+        );
+        const attrs = await readEntry(ctx, entry);
+        // See: https://www.prisma.io/docs/concepts/components/prisma-client/relation-queries#create-multiple-records-and-multiple-related-records
+        // Modify all of the attributes in memory, then replace them all.
+        await ctx.db.$transaction([
+            ctx.db.attributeValue.deleteMany({
+                where: {
+                    entry_id: entry.id,
+                },
+            }),
+            ...attrs
+                .filter((attr) => {
+                    const ATTR_TYPE: string = attr.id.toString();
+                    if (!attributeTypesInOldRDN.has(ATTR_TYPE)) {
+                        return true;
+                    }
+                    const attrSpec = ctx.attributes.get(ATTR_TYPE);
+                    if (!attrSpec?.equalityMatcher) { // If we don't understand the attribute type, we just keep it.
+                        return true;
+                    }
+                    const matcher = attrSpec.equalityMatcher;
+                    const relevantRDNValue = oldrdn.find((atav) => atav.type_.toString() === ATTR_TYPE);
+                    if (!relevantRDNValue) {
+                        return true;
+                    }
+                    return !matcher(relevantRDNValue.value, attr.value);
+                })
+                .map((attr) => ctx.db.attributeValue.create({
+                    data: {
+                        entry_id: entry.id,
+                        type: attr.id.toString(),
+                        tag_class: attr.value.tagClass,
+                        constructed: (attr.value.construction === ASN1Construction.constructed),
+                        tag_number: attr.value.tagNumber,
+                        ber: Buffer.from(attr.value.toBytes()),
+                        ContextValue: {
+                            createMany: {
+                                data: Array.from(attr.contexts.values())
+                                    .flatMap((context) => context.values
+                                        .map((cv) => ({
+                                            // id: null,
+                                            // value_id: null,
+                                            entry_id: entry.id,
+                                            type: context.id.nodes,
+                                            tag_class: cv.tagClass,
+                                            constructed: (cv.construction === ASN1Construction.constructed),
+                                            tag_number: cv.tagNumber,
+                                            ber: Buffer.from(cv.toBytes()),
+                                            // hint: null,
+                                            // jer: null,
+                                            fallback: context.fallback,
+                                        }))),
+                            }
+                        }
+                    },
+                })),
+        ]);
+    }
+
+    entry.rdn = newrdn;
 
     // TODO: If deleteoldrn, delete the matching attribute values.
     return new LDAPResult(
