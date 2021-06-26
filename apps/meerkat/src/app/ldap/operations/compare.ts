e29c0b9b7828469adcefd053daca55cfc0bfd736
Jonathan M. Wilbur <jonathan@wilbur.space>
2021-06-25 22:34:06 -0400

    Finish compare
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- apps/meerkat/src/app/ldap/operations/compare.ts
+++ apps/meerkat/src/app/ldap/operations/compare.ts
@@ -14,7 +14,9 @@ import {
 } from "@wildboar/ldap/src/lib/modules/Lightweight-Directory-Access-Protocol-V3/LDAPResult-resultCode.ta";
 import decodeLDAPDN from "../decodeLDAPDN";
 import findEntry from "../../x500/findEntry";
+import readEntry from "../../database/readEntry";
 import { objectNotFound } from "../results";
+import normalizeAttributeDescription from "@wildboar/ldap/src/lib/normalizeAttributeDescription";
 
 export
 async function compare (
@@ -26,8 +28,22 @@ async function compare (
     if (!entry) {
         return objectNotFound;
     }
-
-    const match = true;
+    const desc = normalizeAttributeDescription(req.ava.attributeDesc);
+    const attrSpec = ctx.attributes.get(desc);
+    if (!attrSpec?.ldapSyntax || !attrSpec.equalityMatcher) {
+        throw new Error();
+    }
+    const syntax = ctx.ldapSyntaxes.get(attrSpec.ldapSyntax);
+    if (!syntax?.decoder) {
+        throw new Error();
+    }
+    const assertedValue = syntax.decoder(req.ava.assertionValue);
+    const matcher = attrSpec.equalityMatcher;
+    const attrs = await readEntry(ctx, entry);
+    const ATTR_TYPE_OID: string = attrSpec.id.toString();
+    const match = attrs
+        .filter((attr) => attr.id.toString() === ATTR_TYPE_OID)
+        .some((attr) => matcher(assertedValue, attr.value));
     return new LDAPResult(
         match
             ? LDAPResult_resultCode_compareTrue
