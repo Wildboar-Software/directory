8cfd6db8d2c90d8b6ba4a36435152b877d4a0516
Jonathan M. Wilbur <jonathan@wilbur.space>
2021-08-17 06:59:27 -0400

    Add LCR and SCR procedures
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

---
+++ apps/meerkat/src/app/distributed/lcrProcedure.ts
@@ -0,0 +1,7 @@
+export
+async function lcrProcedure (): Promise<void> {
+    // Local policy is always no chaining (for now). Easy to implement!
+    return;
+}
+
+export default lcrProcedure;
---
+++ apps/meerkat/src/app/distributed/scrProcedure.ts
@@ -0,0 +1,7 @@
+export
+async function scrProcedure (): Promise<void> {
+    // Local policy is always no chaining (for now). Easy to implement!
+    return;
+}
+
+export default scrProcedure;
