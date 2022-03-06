f2818c22e0a6963c09fa18f0b68785d0db38c803
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-03-05 22:01:05 -0500

    Correct pwdAdminSubentry object class definition

--- apps/meerkat/src/app/init/loadObjectClasses.ts
+++ apps/meerkat/src/app/init/loadObjectClasses.ts
@@ -10,6 +10,27 @@ import {
 } from "@wildboar/x500/src/lib/modules/InformationFramework/ObjectClassKind.ta";
 import { ObjectIdentifier } from "asn1-ts";
 import { AssertionError } from "assert";
+import {
+    pwdAdminSubentry,
+} from "@wildboar/x500/src/lib/modules/InformationFramework/pwdAdminSubentry.oa"
+import { pwdModifyEntryAllowed } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdModifyEntryAllowed.oa"
+import { pwdChangeAllowed } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdChangeAllowed.oa"
+import { pwdMaxAge } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdMaxAge.oa"
+import { pwdExpiryAge } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdExpiryAge.oa"
+import { pwdMinLength } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdMinLength.oa"
+import { pwdVocabulary } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdVocabulary.oa"
+import { pwdAlphabet } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdAlphabet.oa"
+import { pwdDictionaries } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdDictionaries.oa"
+import { pwdExpiryWarning } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdExpiryWarning.oa"
+import { pwdGraces } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdGraces.oa"
+import { pwdFailureDuration } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdFailureDuration.oa"
+import { pwdLockoutDuration } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdLockoutDuration.oa"
+import { pwdMaxFailures } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdMaxFailures.oa"
+import { pwdMaxTimeInHistory } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdMaxTimeInHistory.oa"
+import { pwdMinTimeInHistory } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdMinTimeInHistory.oa"
+import { pwdHistorySlots } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdHistorySlots.oa"
+import { pwdRecentlyExpiredDuration } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdRecentlyExpiredDuration.oa"
+import { pwdEncAlg } from "@wildboar/x500/src/lib/modules/PasswordPolicy/pwdEncAlg.oa"
 
 function prismaOCK2OCK (ock: PrismaObjectClassKind): ObjectClassKind {
     switch (ock) {
@@ -60,6 +81,38 @@ async function loadObjectClasses (ctx: Context): Promise<void> {
             ldapDescription: oc.ldapDescription ?? undefined,
         });
     }
+
+    /**
+     * ITU Recommendation X.501 (2019), Section 14.9 states that subentries of
+     * object class `pwdAdminSubentry` may contain additional attributes that
+     * are not present in the ASN.1 specification. This appears to be a mistake
+     * in the specification. Here, Meerkat DSA's internal representation of the
+     * `pwdAdminSubentry` is overwritten by what is believed to be a correct
+     * definition.
+     */
+    ctx.objectClasses.set(pwdAdminSubentry["&id"].toString(), {
+        ...objectClassFromInformationObject(pwdAdminSubentry),
+        optionalAttributes: new Set([
+            pwdModifyEntryAllowed["&id"].toString(),
+            pwdChangeAllowed["&id"].toString(),
+            pwdMaxAge["&id"].toString(),
+            pwdExpiryAge["&id"].toString(),
+            pwdMinLength["&id"].toString(),
+            pwdVocabulary["&id"].toString(),
+            pwdAlphabet["&id"].toString(),
+            pwdDictionaries["&id"].toString(),
+            pwdExpiryWarning["&id"].toString(),
+            pwdGraces["&id"].toString(),
+            pwdFailureDuration["&id"].toString(),
+            pwdLockoutDuration["&id"].toString(),
+            pwdMaxFailures["&id"].toString(),
+            pwdMaxTimeInHistory["&id"].toString(),
+            pwdMinTimeInHistory["&id"].toString(),
+            pwdHistorySlots["&id"].toString(),
+            pwdRecentlyExpiredDuration["&id"].toString(),
+            pwdEncAlg["&id"].toString(),
+        ]),
+    });
 }
 
 export default loadObjectClasses;
