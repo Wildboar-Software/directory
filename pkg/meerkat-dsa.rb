dfb26bd4ec3c6386c43fff00e2ed6528ce42905f
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-03-18 20:50:46 -0400

    Add packaging-related files

---
+++ pkg/meerkat-dsa.rb
@@ -0,0 +1,21 @@
+class MeerkatDSA < Formula
+    desc "X.500 Directory Server (DSA) and LDAP Server by Wildboar Software"
+    homepage "https://github.com/Wildboar-Software/directory"
+    url "https://github.com/Wildboar-Software/directory/archive/v1.0.0.tar.gz"
+    version = "1.0.0"
+    # sha256 "e86694b2e15d8d4da2477c44e584fb5e860666787d010801199a0a77bcf28a2d"
+
+    def install
+    #   lib.install "./output/libraries/asn1-#{version}.so"
+    #   ln_sf lib/"asn1_#{version}.so", lib/'asn1.so'
+    #   bin.install Dir['./output/executables/*']
+    #   man1.install Dir['./documentation/man/1/*']
+    #   doc.install Dir['./documentation/*.md']
+    #   doc.install Dir['./documentation/html']
+    #   doc.install "./documentation/asn1-#{version}.json"
+    #   doc.install './documentation/mit.license'
+    #   doc.install './documentation/credits.csv'
+    #   doc.install './documentation/releases.csv'
+    end
+
+end
