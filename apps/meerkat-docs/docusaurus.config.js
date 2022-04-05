f6ff4ef17651563ba48e8fa2a186bc1bf66ec289
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-04-05 12:18:51 -0400

    Maybe fix PWA issues with Docusaurus

--- apps/meerkat-docs/docusaurus.config.js
+++ apps/meerkat-docs/docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
                     {
                         tagName: 'link',
                         rel: 'icon',
-                        href: '/img/docusaurus.png',
+                        href: '/directory/img/boar-head-rune-circle.svg',
                     },
                     {
                         tagName: 'link',
