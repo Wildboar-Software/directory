59e60967cb08d9239b69913a46d06f52a3860753
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-04-05 09:31:34 -0400

    Add Google Analytics

--- apps/meerkat-docs/docusaurus.config.js
+++ apps/meerkat-docs/docusaurus.config.js
@@ -35,6 +35,10 @@ const config = {
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
+                googleAnalytics: {
+                    trackingID: 'UA-177382419-1',
+                    anonymizeIP: true,
+                },
             }),
         ],
     ],
