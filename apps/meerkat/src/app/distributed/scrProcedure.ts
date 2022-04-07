b50f035b0f7dfa8fd4d10b3c09e09f4116c5a47e
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-04-07 12:39:34 -0400

    Possibly fix no referrals issue

--- apps/meerkat/src/app/distributed/scrProcedure.ts
+++ apps/meerkat/src/app/distributed/scrProcedure.ts
@@ -21,17 +21,6 @@ async function scrProcedure (
     SRcontinuationList: ContinuationReference[],
 ): Promise<void> {
     // Local policy is always no chaining (for now). Easy to implement!
-    while (SRcontinuationList.length) {
-        /**
-         * I could not find anywhere in the specification where the
-         * SRcontinuationList is ever emptied, but it seems necessary,
-         * otherwise, it will create an infinite loop and grow larger every
-         * time! It seems to be implied that the contents of this list are
-         * consumed by the LCR and SCR procedures, so here, we just empty the
-         * list to simulate "using it."
-         */
-        SRcontinuationList.pop();
-    }
     return;
 }
 
