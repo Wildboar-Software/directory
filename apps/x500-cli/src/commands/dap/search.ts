3185d9ed4b8fe17834c69d1beb14f9db852c5ee0
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-04-18 23:50:20 -0400

    Improve CLI search

--- apps/x500-cli/src/commands/dap/search.ts
+++ apps/x500-cli/src/commands/dap/search.ts
@@ -1,5 +1,5 @@
 import type { Connection, Context } from "../../types";
-import { BIT_STRING, ObjectIdentifier, TRUE, TRUE_BIT } from "asn1-ts";
+import { BIT_STRING, ObjectIdentifier, TRUE_BIT } from "asn1-ts";
 import { DER } from "asn1-ts/dist/node/functional";
 import {
     search,
@@ -28,7 +28,6 @@ import getOptionallyProtectedValue from "@wildboar/x500/src/lib/utils/getOptiona
 import destringifyDN from "../../utils/destringifyDN";
 import printError from "../../printers/Error_";
 import printEntryInformation from "../../printers/EntryInformation";
-import selectAll from "../../utils/selectAll";
 import {
     EntryInformationSelection,
 } from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/EntryInformationSelection.ta";
@@ -46,6 +45,33 @@ import {
     SearchControlOptions_separateFamilyMembers,
     SearchControlOptions_searchFamily,
 } from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/SearchControlOptions.ta";
+import {
+    PagedResultsRequest,
+} from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/PagedResultsRequest.ta";
+import {
+    PagedResultsRequest_newRequest,
+} from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/PagedResultsRequest-newRequest.ta";
+import * as hs from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/HierarchySelections.ta";
+import {
+    ServiceControls,
+} from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/ServiceControls.ta";
+import * as sco from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/ServiceControlOptions.ta";
+import {
+    ServiceControls_priority_low,
+    ServiceControls_priority_medium,
+    ServiceControls_priority_high,
+} from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/ServiceControls-priority.ta";
+import {
+    ServiceControls_scopeOfReferral_country,
+    ServiceControls_scopeOfReferral_dmd,
+} from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/ServiceControls-scopeOfReferral.ta";
+import {
+    FamilyGrouping,
+    FamilyGrouping_entryOnly,
+    FamilyGrouping_strands,
+    FamilyGrouping_multiStrand,
+    FamilyGrouping_compoundEntry,
+} from "@wildboar/x500/src/lib/modules/DirectoryAbstractService/FamilyGrouping.ta";
 
 function subsetFromString (str: string): SearchArgumentData_subset {
     const str_ = str.toLowerCase();
@@ -60,55 +86,227 @@ function subsetFromString (str: string): SearchArgumentData_subset {
     }
 }
 
+function priorityFromString (str: string): ServiceControls["priority"] {
+    switch (str.trim().toLowerCase()) {
+        case ("low"): return ServiceControls_priority_low;
+        case ("medium"): return ServiceControls_priority_medium;
+        case ("high"): return ServiceControls_priority_high;
+        default: return ServiceControls_priority_medium;
+    }
+}
+
+function scopeOfReferralFromString (str: string): ServiceControls["scopeOfReferral"] {
+    switch (str.trim().toLowerCase()) {
+        case ("dmd"): return ServiceControls_scopeOfReferral_dmd;
+        case ("country"): return ServiceControls_scopeOfReferral_country;
+        default: return undefined;
+    }
+}
+
+function familyGroupingFromString (str: string): FamilyGrouping {
+    switch (str.trim().toLowerCase()) {
+        case ("entryOnly"): return FamilyGrouping_entryOnly;
+        case ("compoundEntry"): return FamilyGrouping_compoundEntry;
+        case ("strands"): return FamilyGrouping_strands;
+        case ("multiStrand"): return FamilyGrouping_multiStrand;
+        default: return FamilyGrouping_entryOnly;
+    }
+}
+
 export
 async function search_new (
     ctx: Context,
     conn: Connection,
     argv: any,
 ): Promise<void> {
-    const eis = argv.userAttribute
-        ? new EntryInformationSelection(
-            {
-                select: argv.userAttribute?.map(ObjectIdentifier.fromString),
-            },
-        )
-        : selectAll;
+    const bindDN = destringifyDN(ctx, argv.bindDN ?? "");
     const objectName: DistinguishedName = destringifyDN(ctx, argv.object);
+    const eis = new EntryInformationSelection(
+            argv.userAttribute
+                ? {
+                    select: argv.userAttribute.map(ObjectIdentifier.fromString),
+                }
+                : undefined,
+            argv.typesOnly,
+            argv.extraAttribute
+                ? {
+                    select: argv.extraAttribute.map(ObjectIdentifier.fromString),
+                }
+                : undefined,
+            argv.allContexts
+                ? {
+                    allContexts: null,
+                }
+                : undefined,
+            argv.returnContexts,
+        );
+    const prr: PagedResultsRequest | undefined = argv.pageSize
+        ? {
+            newRequest: new PagedResultsRequest_newRequest(
+                argv.pageSize,
+                argv.sortKey
+                    ? argv.sortKey.map(ObjectIdentifier.fromString)
+                    : undefined,
+                argv.reverse,
+                argv.unmerged,
+                argv.pageNumber,
+            ),
+        }
+        : undefined;
+    const hierarchySelections: hs.HierarchySelections | undefined = argv.hierarchySelections
+        ? (() => {
+            const ret = new Uint8ClampedArray(10);
+            for (const h of argv.hierarchySelections as string[]) {
+                const bit: number = ({
+                    "self": hs.HierarchySelections_self,
+                    "children": hs.HierarchySelections_children,
+                    "parent": hs.HierarchySelections_parent,
+                    "hierarchy": hs.HierarchySelections_hierarchy,
+                    "top": hs.HierarchySelections_top,
+                    "subtree": hs.HierarchySelections_subtree,
+                    "siblings": hs.HierarchySelections_siblings,
+                    "siblingChildren": hs.HierarchySelections_siblingChildren,
+                    "siblingSubtree": hs.HierarchySelections_siblingSubtree,
+                    "all": hs.HierarchySelections_all,
+                }[h]) ?? hs.HierarchySelections_self;
+                ret[bit] = TRUE_BIT;
+            }
+            return ret;
+        })()
+        : undefined;
     const searchOptions: BIT_STRING = new Uint8ClampedArray(12);
+    if (argv.searchAliases) {
+        searchOptions[SearchControlOptions_searchAliases] = TRUE_BIT;
+    }
+    if (argv.matchedValuesOnly) {
+        searchOptions[SearchControlOptions_matchedValuesOnly] = TRUE_BIT;
+    }
+    if (argv.checkOverspecified) {
+        searchOptions[SearchControlOptions_checkOverspecified] = TRUE_BIT;
+    }
+    if (argv.performExactly) {
+        searchOptions[SearchControlOptions_performExactly] = TRUE_BIT;
+    }
+    if (argv.includeAllAreas) {
+        searchOptions[SearchControlOptions_includeAllAreas] = TRUE_BIT;
+    }
+    if (argv.noSystemRelaxation) {
+        searchOptions[SearchControlOptions_noSystemRelaxation] = TRUE_BIT;
+    }
+    if (argv.dnAttribute) {
+        searchOptions[SearchControlOptions_dnAttribute] = TRUE_BIT;
+    }
+    if (argv.matchOnResidualName) {
+        searchOptions[SearchControlOptions_matchOnResidualName] = TRUE_BIT;
+    }
+    if (argv.entryCount) {
+        searchOptions[SearchControlOptions_entryCount] = TRUE_BIT;
+    }
+    if (argv.useSubset) {
+        searchOptions[SearchControlOptions_useSubset] = TRUE_BIT;
+    }
     if (argv.separateFamilyMembers) {
         searchOptions[SearchControlOptions_separateFamilyMembers] = TRUE_BIT;
     }
+    if (argv.searchFamily) {
+        searchOptions[SearchControlOptions_searchFamily] = TRUE_BIT;
+    }
+    const serviceControlOptions: sco.ServiceControlOptions = new Uint8ClampedArray(15);
+    if (argv.preferChaining) {
+        serviceControlOptions[sco.ServiceControlOptions_preferChaining] = TRUE_BIT;
+    }
+    if (argv.chainingProhibited) {
+        serviceControlOptions[sco.ServiceControlOptions_chainingProhibited] = TRUE_BIT;
+    }
+    if (argv.localScope) {
+        serviceControlOptions[sco.ServiceControlOptions_localScope] = TRUE_BIT;
+    }
+    if (argv.dontUseCopy) {
+        serviceControlOptions[sco.ServiceControlOptions_dontUseCopy] = TRUE_BIT;
+    }
+    if (argv.dontDereferenceAliases) {
+        serviceControlOptions[sco.ServiceControlOptions_dontDereferenceAliases] = TRUE_BIT;
+    }
+    if (argv.subentries) {
+        serviceControlOptions[sco.ServiceControlOptions_subentries] = TRUE_BIT;
+    }
+    if (argv.copyShallDo) {
+        serviceControlOptions[sco.ServiceControlOptions_copyShallDo] = TRUE_BIT;
+    }
+    if (argv.partialNameResolution) {
+        serviceControlOptions[sco.ServiceControlOptions_partialNameResolution] = TRUE_BIT;
+    }
+    if (argv.manageDSAIT) {
+        serviceControlOptions[sco.ServiceControlOptions_manageDSAIT] = TRUE_BIT;
+    }
+    if (argv.noSubtypeMatch) {
+        serviceControlOptions[sco.ServiceControlOptions_noSubtypeMatch] = TRUE_BIT;
+    }
+    if (argv.noSubtypeSelection) {
+        serviceControlOptions[sco.ServiceControlOptions_noSubtypeSelection] = TRUE_BIT;
+    }
+    if (argv.countFamily) {
+        serviceControlOptions[sco.ServiceControlOptions_countFamily] = TRUE_BIT;
+    }
+    if (argv.dontSelectFriends) {
+        serviceControlOptions[sco.ServiceControlOptions_dontSelectFriends] = TRUE_BIT;
+    }
+    if (argv.dontMatchFriends) {
+        serviceControlOptions[sco.ServiceControlOptions_dontMatchFriends] = TRUE_BIT;
+    }
+    if (argv.allowWriteableCopy) {
+        serviceControlOptions[sco.ServiceControlOptions_allowWriteableCopy] = TRUE_BIT;
+    }
+    const serviceControls = new ServiceControls(
+        serviceControlOptions,
+        argv.priority
+            ? priorityFromString(argv.priority)
+            : undefined,
+        argv.timeLimit,
+        argv.sizeLimit,
+        argv.scopeOfReferral
+            ? scopeOfReferralFromString(argv.scopeOfReferral)
+            : undefined,
+        argv.attributeSizeLimit,
+        undefined, // FIXME:
+        argv.serviceType
+            ? ObjectIdentifier.fromString(argv.serviceType)
+            : undefined,
+        argv.userClass,
+    );
     const reqData: SearchArgumentData = new SearchArgumentData(
         {
             rdnSequence: objectName,
         },
         subsetFromString(argv.subset),
+        undefined, // This will be implemented as an LDAP filter eventually.
         undefined,
-        TRUE,
         eis,
+        prr,
         undefined,
         undefined,
+        argv.checkOverspecified,
         undefined,
-        TRUE,
-        undefined,
-        undefined,
-        undefined,
+        argv.extendedArea,
+        hierarchySelections,
         searchOptions,
-        undefined,
-        undefined,
+        undefined, // Joins unsupported.
+        undefined, // Joins unsupported.
         [],
+        serviceControls,
+        undefined, // FIXME: security parameters
+        bindDN,
+        undefined,
+        undefined, // FIXME: Fill in criticalExtensions
         undefined,
         undefined,
         undefined,
         undefined,
         undefined,
         undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        undefined,
+        argv.familyGrouping
+            ? familyGroupingFromString(argv.familyGrouping)
+            : undefined,
     );
     const arg: SearchArgument = {
         unsigned: reqData,
--- apps/x500-cli/src/yargs/dap_search.ts
+++ apps/x500-cli/src/yargs/dap_search.ts
@@ -64,13 +64,15 @@ function create (ctx: Context): CommandModule {
                     describe: "DSA should check if filter is overspecified",
                 })
                 .option("extendedArea", {
-                    alias: "x",
+                    alias: "a",
                     type: "number",
                     describe: "The extended area to search",
                 })
+                // REVIEW: I don't know if this will work.
+                // Meaning: combining arrays with enums.
                 .options("hierarchySelections", {
                     alias: "h",
-                    type: "string",
+                    type: "array",
                     describe: "Which members of a hierarchy to select",
                     choices: [
                         "self",
@@ -84,24 +86,7 @@ function create (ctx: Context): CommandModule {
                         "siblingSubtree",
                         "all",
                     ],
-                    default: "self",
                 })
-                // Already covered
-                // .option("searchAliases", {
-                //     type: "boolean",
-                //     describe: "",
-                //     default: false,
-                // })
-                // .option("matchedValuesOnly", {
-                //     type: "boolean",
-                //     describe: "",
-                //     default: false,
-                // })
-                // .option("checkOverspecified", {
-                //     type: "boolean",
-                //     describe: "",
-                //     default: false,
-                // })
                 .option("performExactly", {
                     alias: "e",
                     type: "boolean",
@@ -156,7 +141,151 @@ function create (ctx: Context): CommandModule {
                     type: "array",
                     describe: "Dot-delimited object identifier of attribute type to select",
                 })
-                // TODO: EIS options: extraAttribute, familyReturn, etc.
+                .option("extraAttribute", {
+                    alias: "x",
+                    type: "array",
+                    describe: "Dot-delimited object identifier of operational attribute type to select",
+                })
+                .option("typesOnly", {
+                    alias: "t",
+                    type: "boolean",
+                    describe: "Whether to return only attribute types (no attribute values)",
+                })
+                .option("allContexts", {
+                    type: "boolean",
+                    describe: "Whether to ignore context assertion defaults",
+                })
+                .option("returnContexts", {
+                    alias: "c",
+                    type: "boolean",
+                    describe: "Whether to return contexts with values",
+                })
+                .option("familyReturn", {
+                    type: "string",
+                    choices: [
+                        "contributing",
+                        "participating",
+                        "compound",
+                    ],
+                    description: "The type of familyReturn to use"
+                })
+                .option("familySelect", {
+                    alias: "f",
+                    type: "string",
+                    description: "Dot-delimited object identifier of family to return in compound entries",
+                })
+
+                // ServiceControlOptions
+                .option("preferChaining", {
+                    type: "boolean",
+                    description: "Whether to ask the DSA to chain the operation to other DSAs rather than returning referrals",
+                })
+                .option("chainingProhibited", {
+                    type: "boolean",
+                    description: "Whether to explicitly prohibit chaining to other DSAs",
+                })
+                .option("localScope", {
+                    type: "boolean",
+                    description: "Whether the operation should be limited to a local scope",
+                })
+                .option("dontUseCopy", {
+                    type: "boolean",
+                    description: "Whether to permit the use of shadow DSEs or writeable copies.",
+                })
+                .option("dontDereferenceAliases", {
+                    type: "boolean",
+                    description: "Whether to prohibit dereferencing aliases",
+                })
+                .option("subentries", {
+                    type: "boolean",
+                    description: "Whether to only display subentries",
+                })
+                .option("copyShallDo", {
+                    type: "boolean",
+                    description: "Whether the DSA shall use a shadow DSE even if it is only able to partially satisfy the query",
+                })
+                .option("partialNameResolution", {
+                    type: "boolean",
+                    description: "Whether the DSA shall settle for the highest found entry if the target entry is not found.",
+                })
+                .option("manageDSAIT", {
+                    type: "boolean",
+                    description: "Whether the request shall break the illusion of a single unified directory and permit modification of operational attributes and knowledge references.",
+                })
+                .option("noSubtypeMatch", {
+                    type: "boolean",
+                    description: "Whether attribute subtypes shall not be matched",
+                })
+                .option("noSubtypeSelection", {
+                    type: "boolean",
+                    description: "Whether attribute subtypes shall not be selected",
+                })
+                .option("countFamily", {
+                    type: "boolean",
+                    description: "Whether each member of a compound entry should count towards size limits",
+                })
+                .option("dontSelectFriends", {
+                    type: "boolean",
+                    description: "Whether friend attributes should not be selected",
+                })
+                .option("dontMatchFriends", {
+                    type: "boolean",
+                    description: "Whether friend attributes should not be matched",
+                })
+                .option("allowWriteableCopy", {
+                    type: "boolean",
+                    description: "Whether a DSE of type writeableCopy is acceptable in the provision of a query service request.",
+                })
+
+                // Service controls
+                .option("priority", {
+                    type: "string",
+                    choices: [
+                        "low",
+                        "medium",
+                        "high",
+                    ],
+                    description: "The priority of the request",
+                })
+                .option("timeLimit", {
+                    type: "number",
+                    description: "How many seconds the request should be permitted to last before being aborted",
+                })
+                .option("sizeLimit", {
+                    type: "number",
+                    description: "The maximum number of entries the request may return",
+                })
+                .option("scopeOfReferral", {
+                    type: "string",
+                    choices: [
+                        "dmd",
+                        "country",
+                    ],
+                    description: "The subset of referrals permitted to be returned",
+                })
+                .option("attributeSizeLimit", {
+                    type: "number",
+                    description: "The maximum permitted size of attributes to be returned"
+                })
+                // TODO: manageDSAITPlaneRef
+                .option("serviceType", {
+                    type: "string",
+                    description: "Dot-delimited object identifier of the search service to request",
+                })
+                .option("userClass", {
+                    type: "number",
+                    description: "The search service user class to request"
+                })
+                .option("familyGrouping", {
+                    type: "string",
+                    choices: [
+                        "entryOnly",
+                        "compoundEntry",
+                        "strands",
+                        "multiStrand",
+                    ],
+                    description: "How families of entries in a compound entry are combined for the sake of operation evaluation"
+                })
                 .help()
                 ;
         },
