5e0af39d87336e3de1c1ff1d59f7a21c9d8b0a87
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-08-02 22:07:24 -0400

    Create fewer random entries

--- apps/create-test-dit/src/app/create-gb.ts
+++ apps/create-test-dit/src/app/create-gb.ts
@@ -1157,7 +1157,7 @@ async function seedGB (
     }
 
     // Create random people
-    for (let i = 0; i < 10000; i++) {
+    for (let i = 0; i < 1000; i++) {
         const [ rdn, attributes, cn ] = createMockPersonAttributes();
         const dn: DistinguishedName = [
             ...baseObject,
