f31a30f07ef375a4d03b0b15088c1cf5fc0cc68f
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-08-15 15:33:41 -0400

    Implement redirectionOrDLExpansionSingleElementMatch

---
+++ apps/meerkat/src/app/matching/equality/redirectionOrDLExpansionSingleElementMatch.ts
@@ -0,0 +1,44 @@
+import type { Context } from "@wildboar/meerkat-types";
+import { ASN1Element, BERElement } from "asn1-ts";
+import type { EqualityMatcher } from "@wildboar/x500";
+import { oRNameSingleElementMatch } from "./oRNameSingleElementMatch";
+
+// DLExpansionHistory ::= SEQUENCE SIZE (1..ub-dl-expansions) OF DLExpansion
+
+// DLExpansion ::= SEQUENCE {
+//   dl                 ORAddressAndOptionalDirectoryName,
+//   dl-expansion-time  Time
+// }
+
+// RedirectionHistory ::= SEQUENCE SIZE (1..ub-redirections) OF Redirection
+
+// Redirection ::= SEQUENCE {
+//   intended-recipient-name  IntendedRecipientName,
+//   redirection-reason       RedirectionReason
+// }
+
+// IntendedRecipientName ::= SEQUENCE {
+//   intended-recipient  ORAddressAndOptionalDirectoryName,
+//   redirection-time    Time
+// }
+
+// TODO: This could be made more efficient by not decoding the assertion every time.
+export
+const redirectionOrDLExpansionSingleElementMatch: EqualityMatcher = (
+    assertion: ASN1Element,
+    value: ASN1Element,
+): boolean => {
+    const values = value.sequenceOf.map((el) => {
+        const firstComponent = new BERElement();
+        firstComponent.fromBytes(el.value);
+        return firstComponent;
+    });
+    for (const v of values) {
+        if (oRNameSingleElementMatch(assertion, v)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+export default redirectionOrDLExpansionSingleElementMatch;
