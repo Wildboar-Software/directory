2508cdcbd290910724a177de26bad2d0845fbd25
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-08-15 14:28:16 -0400

    Implement oRAddressSubstringElementsMatch

---
+++ apps/meerkat/src/app/matching/equality/oRAddressSubstringElementsMatch.spec.ts
@@ -0,0 +1,84 @@
+import { isSubstringSubset } from "./oRAddressSubstringElementsMatch";
+
+describe("isSubstringSubset()", () => {
+    test("#1", () => {
+        const value = {
+            asdf: "zxcv",
+            qwer: "yuiop",
+        };
+        const assertion = {
+            asdf: "xc",
+            qwer: "op",
+        };
+        expect(isSubstringSubset(value, assertion)).toBe(true);
+    });
+    test("#2", () => {
+        const value = {
+            asdf: "zxcv",
+            qwer: "yuiop",
+        };
+        const assertion = {
+            asdf: "xc",
+            qwer: "yugioh... am I right???",
+        };
+        expect(isSubstringSubset(value, assertion)).toBe(false);
+    });
+    test("#3", () => {
+        const value = {
+            asdf: "zxcv",
+            qwer: {
+                big: "boi",
+            },
+        };
+        const assertion = {
+            asdf: "xc",
+            qwer: {
+                big: "oi",
+            },
+        };
+        expect(isSubstringSubset(value, assertion)).toBe(true);
+    });
+    test("#4", () => {
+        const value = {
+            asdf: "zxcv",
+            qwer: {
+                big: "boi",
+            },
+        };
+        const assertion = {
+            asdf: "xc",
+            qwer: {
+                big: "chungus",
+            },
+        };
+        expect(isSubstringSubset(value, assertion)).toBe(false);
+    });
+    test("#5", () => {
+        const value = {
+            asdf: "zxcv",
+            qwer: {
+                big: "boi",
+            },
+        };
+        const assertion = {
+            asdf: "zxcv",
+            qwer: {
+                big: "boi",
+            },
+        };
+        expect(isSubstringSubset(value, assertion)).toBe(true);
+    });
+    test("#6", () => {
+        const value = {
+            asdf: "zxcv",
+            qwer: {
+                big: "boi",
+            },
+        };
+        const assertion = {
+            asdf: "zxcv",
+            qwer: [ "big", "boi" ],
+        };
+        expect(isSubstringSubset(value, assertion)).toBe(false);
+    });
+});
