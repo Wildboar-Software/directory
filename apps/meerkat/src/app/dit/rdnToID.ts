66231b2a867a81e31dff52b91171ae3f3b1e4f82
Jonathan M. Wilbur <jonathan@wilbur.space>
2023-01-21 22:01:05 -0500

    Use normalized RDN search in rdnToID

--- apps/meerkat/src/app/dit/rdnToID.ts
+++ apps/meerkat/src/app/dit/rdnToID.ts
@@ -1,5 +1,5 @@
 import type { Context } from "@wildboar/meerkat-types";
-import { ObjectIdentifier, BERElement, ASN1Construction } from "asn1-ts";
+import { BERElement, ASN1Construction, OBJECT_IDENTIFIER } from "asn1-ts";
 import type {
     RelativeDistinguishedName as RDN,
 } from "@wildboar/x500/src/lib/modules/InformationFramework/RelativeDistinguishedName.ta";
@@ -10,6 +10,8 @@ import {
 import compareRDN from "@wildboar/x500/src/lib/comparators/compareRelativeDistinguishedName";
 import getNamingMatcherGetter from "../x500/getNamingMatcherGetter";
 import { getEntryExistsFilter } from "../database/entryExistsFilter";
+import getEqualityNormalizer from "../x500/getEqualityNormalizer";
+import { Prisma } from "@prisma/client";
 
 /**
  * @summary Determine the database ID of a subordinate by its RDN
@@ -35,26 +37,50 @@ async function rdnToID (
     superior_id: number,
     rdn: RDN,
 ): Promise<number | undefined> {
+    const NORMALIZER_GETTER = getEqualityNormalizer(ctx);
+    let use_normalized_rdn_search: boolean = true;
+    const atav_strs: [type: OBJECT_IDENTIFIER, value: string][] = [];
+    for (const atav of rdn) {
+        const normalized_value: string | undefined = NORMALIZER_GETTER(atav.type_)?.(ctx, atav.value);
+        if (!normalized_value) {
+            use_normalized_rdn_search = false;
+            break;
+        }
+        atav_strs.push([ atav.type_, normalized_value ]);
+    }
     const exactMatch = await ctx.db.entry.findFirst({
-        where: {
-            immediate_superior_id: superior_id,
-            ...getEntryExistsFilter(),
-            AND: rdn.map((atav) => ({
-                RDN: {
-                    some: {
-                        type_oid: atav.type_.toBytes(),
-                        tag_class: atav.value.tagClass,
-                        constructed: (atav.value.construction === ASN1Construction.constructed),
-                        tag_number: atav.value.tagNumber,
-                        content_octets: Buffer.from(
-                            atav.value.value.buffer,
-                            atav.value.value.byteOffset,
-                            atav.value.value.byteLength,
-                        ),
+        where: use_normalized_rdn_search
+            ? {
+                immediate_superior_id: superior_id,
+                ...getEntryExistsFilter(),
+                AND: atav_strs.map(([ type_, str ]): Prisma.EntryWhereInput => ({
+                    RDN: {
+                        some: {
+                            type_oid: type_.toBytes(),
+                            normalized_str: str,
+                        },
                     },
-                },
-            })),
-        },
+                })),
+            }
+            : {
+                immediate_superior_id: superior_id,
+                ...getEntryExistsFilter(),
+                AND: rdn.map((atav) => ({
+                    RDN: {
+                        some: {
+                            type_oid: atav.type_.toBytes(),
+                            tag_class: atav.value.tagClass,
+                            constructed: (atav.value.construction === ASN1Construction.constructed),
+                            tag_number: atav.value.tagNumber,
+                            content_octets: Buffer.from(
+                                atav.value.value.buffer,
+                                atav.value.value.byteOffset,
+                                atav.value.value.byteLength,
+                            ),
+                        },
+                    },
+                })),
+            },
         select: {
             id: true,
         },
