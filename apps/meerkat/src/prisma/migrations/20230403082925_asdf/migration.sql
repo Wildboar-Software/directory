48d6afefaa4e65a48c775908091fd9fc65769dce
Jonathan M. Wilbur <jonathan@wilbur.space>
2023-04-03 04:31:50 -0400

    Drop nsk column from operational bindings

---
+++ apps/meerkat/src/prisma/migrations/20230403082925_asdf/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `nsk_group` on the `OperationalBinding` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `OperationalBinding` DROP COLUMN `nsk_group`;
