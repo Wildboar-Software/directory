4a424af488c51fb023259742abed59724a7e3ecb
Jonathan M. Wilbur <jonathan@wilbur.space>
2023-07-02 14:47:00 -0400

    chore: comment testing shadow command

--- test/iter.sh
+++ test/iter.sh
@@ -4,3 +4,5 @@
 docker compose -f ./test/docker-compose.yaml down
 nx run meerkat:build
 docker compose -f ./test/docker-compose.yaml up --build
+
+# node ./dist/apps/x500-cli/main.js dop supply shadow 'c=US' 'cn=dsa2' --naddr=idm://dsa2:4632 --supplier-initiated --all-contexts --supply-all-contexts --other-times --window-size=120 --update-interval=300
