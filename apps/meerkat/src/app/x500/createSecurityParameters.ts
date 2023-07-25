05984cd5467ae2c9705904ccd2c3234230402d51
Jonathan M. Wilbur <jonathan@wilbur.space>
2023-07-24 20:56:16 -0400

    fix: always sign DISP requests, even if in bulk insert mode

--- apps/meerkat/src/app/x500/createSecurityParameters.ts
+++ apps/meerkat/src/app/x500/createSecurityParameters.ts
@@ -34,11 +34,12 @@ function createSecurityParameters (
     // This was added as a result of a deeply-entrenched design mistake. This
     // basically forces signing, even if bulk insert mode is on. This is so a
     // DSA can still chain with a signed argument.
-    dsp: boolean = false,
+    demandSigning: boolean = false,
 ): SecurityParameters | undefined {
     const signingCertPath = ctx.config.signing.certPath;
     const isDopOperation = operationCode && ("local" in operationCode) && (operationCode.local >= 100);
-    const signable: boolean = !!(dsp && signingCertPath) || !!(signingCertPath && signed && (!ctx.config.bulkInsertMode || isDopOperation));
+    const signable: boolean = !!(demandSigning && signingCertPath)
+        || !!(signingCertPath && signed && (!ctx.config.bulkInsertMode || isDopOperation));
     // Errors are thrown by the server, not client.
     // This block is a short-circuit to prevent DoS.
     if (!signable && errorCode) {
