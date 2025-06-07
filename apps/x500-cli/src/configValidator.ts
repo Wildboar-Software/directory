c58d71e410e9650b9cc60f371fe599ea8a30d6a3
Jonathan M. Wilbur <jonathan@wilbur.space>
2025-06-06 21:18:21 -0400

    chore: replace webpack with esbuild

--- apps/x500-cli/src/configValidator.ts
+++ apps/x500-cli/src/configValidator.ts
@@ -1,16 +1,17 @@
-import ajv from "ajv";
+import * as ajv from "ajv";
 import { schema } from "@wildboar/x500-cli-config";
 
-const ajvInstance: ajv.Ajv = ajv({
+const ajvInstance: ajv.Ajv = new ajv.Ajv({
     useDefaults: true,
 });
-ajvInstance.addKeyword("unicodePattern", {
+ajvInstance.addKeyword({
     validate: (schema: any, data: any): boolean => (
         (typeof schema === "string" && typeof data === "string")
             ? (new RegExp(schema, "u")).test(data) : false
     ),
     async: true,
     errors: false,
+    keyword: "unicodePattern",
 });
 
 export default ajvInstance.compile(schema);
