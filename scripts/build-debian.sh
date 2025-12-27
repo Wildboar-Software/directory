7de46aab564ac2d1b864164f4b329f273abc5b5b
Jonathan M. Wilbur <jonathan@wilbur.space>
2025-12-27 15:54:59 -0500

    chore: remove obsolete comments

--- scripts/build-debian.sh
+++ scripts/build-debian.sh
@@ -7,14 +7,3 @@ git archive \
   --prefix=meerkat-dsa-${VERSION}/ \
   HEAD \
 | xz -c > ../meerkat-dsa_${VERSION}.orig.tar.xz
-
-# tar --exclude=debian \
-#     --exclude=node_modules \
-#     --exclude=dist \
-#     --exclude=.nx \
-#     --exclude=.vscode \
-#     --exclude=demo \
-#     --exclude=tmp \
-#     --exclude=
-#     -cJf ../meerkat-dsa_${VERSION}.orig.tar.xz \
-#     .
