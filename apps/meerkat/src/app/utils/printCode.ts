718529e96848fff4ed9265d1d2decfd941aa3563
Jonathan M. Wilbur <jonathan@wilbur.space>
2026-02-19 07:02:39 -0500

    chore(meerkat): use printCode instead of codeToString

--- apps/meerkat/src/app/utils/printCode.ts
+++ apps/meerkat/src/app/utils/printCode.ts
@@ -1,29 +1,3 @@
-import type {
-    Code,
-} from "@wildboar/x500/CommonProtocolSpecification";
-
-/**
- * @summary Convert a `Code` into a human-readable string.
- * @description
- *
- * Converts an X.500 `Code` into a human-readable string. If the code uses the
- * `global` alternative, the object identifier will be displayed in
- * dot-delimited form.
- *
- * @param code The code to convert to a string.
- * @returns The string representation of the code
- *
- * @function
- */
-export
-function printCode (code: Code): string {
-    if ("local" in code) {
-        return code.local.toString();
-    } else if ("global" in code) {
-        return code.global.toString();
-    } else {
-        return code.toString();
-    }
-}
-
-export default printCode;
+import { codeToString } from "@wildboar/x500";
+export { codeToString as printCode } from "@wildboar/x500";
+export default codeToString;
